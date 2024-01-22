3a2ce1ef3addff608dde12abdd8d4e86368c13b4
Jonathan Parry <148205201+JonathanParryArbor@users.noreply.github.com>
2024-01-22 15:44:50 +0000

    Create undrop.sql

---
+++ sql_queries/custom_warehouse/undrop.sql
@@ -0,0 +1,13 @@
+-- Undrop allows for certain objects in Snowflake to be restored, as long as they were dropped within the Time Travel Data retention period
+-- This retention period is 24 hours by default
+
+-- Undrop a database
+UNDROP DATABASE DEMO_DB;
+
+-- Undrop a schema
+UNDROP SCHEMA DEMO_DB.RAW;
+
+-- Undrop a Table
+UNDROP TABLE DEMO_DB.RAW.USERS;
+
+-- Further information can be found here: https://docs.snowflake.com/en/sql-reference/sql/undrop
